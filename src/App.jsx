4e07bc4c2ef5035bef7811c6d2799b9eb8aac21a
Tranel <tranel@Tranels-MacBook-Air.local>
2023-07-26 21:54:37 -0500

    Updated BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import AboutMe from "./pages/AboutMe";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
-import Resume from "./pages/Resume";
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
-      <main className="content">
-        <Routes>
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/resume" component={Resume} />
-          <Route path="/" component={AboutMe} />
-        </Routes>
-      </main>
+      <Outlet />
       <Footer />
-    </Router>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <RouterProvider router={router} />
+);
+
