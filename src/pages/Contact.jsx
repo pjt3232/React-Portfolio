73b4896f31cc96b13b182d6e652b0b11fd435077
Tranel <tranel@Tranels-MacBook-Air.local>
2023-07-27 19:35:46 -0500

    Updated Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,79 @@
-import React from  "react";
+import React, { useState } from  "react";
 
 // contact page with form and input
 const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const handleEmailChange = (event) => {
+        const newEmail = event.target.value;
+        setEmail(newEmail);
+
+        if (newEmail && !validateEmail(newEmail)) {
+            setEmailError('Please enter a valid email address');
+        } else {
+            setEmailError('');
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!name || !email || !message) {
+            alert('All fields are required');
+            return;
+        }
+
+        if (!validateEmail(email)) {
+            alert('Please enter a valid email address');
+            return;
+        }
+
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
     return (
         <section className="container content contact">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
-                    <label>Name:</label>
-                    <input type="text" required />
+                    <label htmlFor="name">Name:</label>
+                    <input 
+                    type="text" 
+                    id="name" 
+                    value={name} 
+                    onChange={(event) => setName(event.target.value)}
+                    required 
+                    />
                 </div>
                 <div>
-                    <label>Email:</label>
-                    <input type="email" required />
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                    type="email" 
+                    id="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required 
+                    />
+                    {emailError && <p>{emailError}</p>}
                 </div>
                 <div>
-                    <label>Message:</label>
-                    <input as="textarea" required />
+                    <label htmlFor="message">Message:</label>
+                    <textarea 
+                    id="message"
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                    required 
+                    />
                 </div>
-                <button className="btn-primary" variant="primary" type="submit">Submit</button>
+                <button className="btn-primary" type="submit">Submit</button>
             </form>
         </section>
     );
