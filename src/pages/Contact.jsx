5e33cba419918365489eee062eff64844af457b8
Tranel <tranel@Tranels-MacBook-Air.local>
2023-08-31 16:22:14 -0500

    Updated portfolio and contact page; major changes to css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,79 +1,10 @@
-import React, { useState } from  "react";
+import React from  "react";
 
 // contact page with form and input
 const Contact = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [emailError, setEmailError] = useState('');
-
-    const validateEmail = (email) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    };
-
-    const handleEmailChange = (event) => {
-        const newEmail = event.target.value;
-        setEmail(newEmail);
-
-        if (newEmail && !validateEmail(newEmail)) {
-            setEmailError('Please enter a valid email address');
-        } else {
-            setEmailError('');
-        }
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (!name || !email || !message) {
-            alert('All fields are required');
-            return;
-        }
-
-        if (!validateEmail(email)) {
-            alert('Please enter a valid email address');
-            return;
-        }
-
-        setName('');
-        setEmail('');
-        setMessage('');
-    };
 
     return (
         <section className="container content contact">
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input 
-                    type="text" 
-                    id="name" 
-                    value={name} 
-                    onChange={(event) => setName(event.target.value)}
-                    required 
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input 
-                    type="email" 
-                    id="email"
-                    value={email}
-                    onChange={handleEmailChange}
-                    required 
-                    />
-                    {emailError && <p>{emailError}</p>}
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea 
-                    id="message"
-                    value={message}
-                    onChange={(event) => setMessage(event.target.value)}
-                    required 
-                    />
-                </div>
-                <button className="btn-primary" type="submit">Submit</button>
                 <div className="contact-information">
                     <h2>You Can Reach Me Here</h2>
                     <div>
@@ -84,8 +15,15 @@ const Contact = () => {
                         <label htmlFor="phone">Phone Number:</label>
                         <a href="tel:608-509-2031">608-509-2031</a>
                     </div>
+                    <div>
+                        <label htmlFor="phone">LinkedIn:</label>
+                        <a href="https://www.linkedin.com/in/peyton-tranel/">Peyton Tranel</a>
+                    </div>
+                    <div>
+                        <label htmlFor="phone">Resume:</label>
+                        <a href="https://drive.google.com/file/d/1L12gwHwC6VXInYsOa8Omqnm9jUDzYhsU/view?usp=sharing">Peyton J Tranel Resume</a>
+                    </div>
                 </div>
-            </form>
         </section>
     );
 };
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,46 +6,52 @@ import { Link } from "react-router-dom";
 const Portfolio = () => {
     const projects = [
         {
-            title: "TechPrep-Pro",
-            imageUrl: "https://i.postimg.cc/v87xqnfc/Tech-Prep-Pro-Screenshot.png",
-            deployedLink: "https://techprep-pro-4b8ef967b7cd.herokuapp.com/",
-            githubLink: "https://github.com/annabellefosburgh/dangerous-wombat"
-        },
-        {
             title: "Ecopaw Tracker",
             imageUrl: "https://dochub.com/20peytra/mqNjP3BVW1O6k7PR9yGzLk/ecopaw-tracker-screenshot-png?dt=DH4cqfW3AsjXGbTC8yqo",
             deployedLink: "https://ecopaw-tracker-033283e6e802.herokuapp.com/",
-            githubLink: "https://github.com/MCunha17/ecopaw-tracker"
+            githubLink: "https://github.com/MCunha17/ecopaw-tracker",
+            problem: "The problem solved with this application was wanting to track the users carbon emissions in an effort to save polar bears. This was solved by allowing users to create an account to log their carbon emissions and providing multiple donation websites to donate to polar bear conservation funds.",
+            technologies: "Carbon Footprint API, bcrypt, Express, Handlebars templates, sequelize, and MySQL"
         },
         {
             title: "Thirst Traps",
             imageUrl: "https://user-images.githubusercontent.com/124208861/233245572-31c3d049-8850-45d8-8eae-6c9f804bc93c.png",
             deployedLink: "https://annabellefosburgh.github.io/heavy-dolphin/",
-            githubLink: "https://github.com/annabellefosburgh/heavy-dolphin"
+            githubLink: "https://github.com/annabellefosburgh/heavy-dolphin",
+            problem: "The problem solved with this application was wanting to find a new drink to try based on a certain criteria the user wants. This was solved by creating a drop down menu for three separate criteria, which were alcoholic/non-alcoholic , the spirit type, and category of drink. Given that criteria it picked a drink for the user and provides a YouTube video on how to make that drink.",
+            technologies: "TheCocktailDB API, YouTube IFrame Player API, and Picnic CSS framework"
         },
         {
             title: "PWA Text Editor",
             imageUrl: "https://dochub.com/20peytra/7J4mQvgRv6o83g0Rj2pO5n/pwa-text-editor-screenshot-png?dt=uDZ_atmy7D6sSxEbPckZ",
             deployedLink: "https://jate-quill-892f91be6a60.herokuapp.com/",
-            githubLink: "https://github.com/pjt3232/PWA-Text-Editor"
+            githubLink: "https://github.com/pjt3232/PWA-Text-Editor",
+            problem: "The problem solved with this application is wanting a simple text editor that doesn't have overwhelming features, but works when the user is offline. This was solved by allowing the user to write text that is then saved to the user's computer storage and by adding offline functionality. Every time the user wants to use the app it will have their previous data and work with internet connection or without. This app also has the option to download it directly to the user's computer.",
+            technologies: "PWAs, WebpackPwaManifest, Inject Manifest, IndexedDB, service workers, and workbox npm packages"
         },
         {
             title: "NoSQL Social Network",
             imageUrl: "https://dochub.com/20peytra/8adOrbPVQEDDXJrV24Mj7D/nosql-social-network-screenshot-png?dt=uLUaX4urSc34zsyjmbCi",
             deployedLink: "https://drive.google.com/file/d/1NpvXVn7bcMkF0VJZVlX5-6kWFfL4PmN6/view",
-            githubLink: "https://github.com/pjt3232/NoSQL-Social-Network"
+            githubLink: "https://github.com/pjt3232/NoSQL-Social-Network",
+            problem: "The problem solved with this application is wanting to create the back-end calls for the server in an application that will eventually be a social networking app. This was solved by creating the back-end functionality for users, their posted thoughts, and another user's reactions to those thoughts. With these back-end calls, the user can create, update, read, and/or delete any user, thought, or reaction.",
+            technologies: "NoSQL, MongoDB, and Express"
         },
         {
             title: "Business Database",
             imageUrl: "https://dochub.com/20peytra/mqNjP3BVW1OOAQWR9yGzLk/business-database-screenshot-png?dt=StBJagQRkTe5zy66yeFy",
             deployedLink: "https://drive.google.com/file/d/1uyoBdPSWTIw5QIYcmSfOK5386Hb3zmCl/view?usp=sharing",
-            githubLink: "https://github.com/pjt3232/Business-Database"
+            githubLink: "https://github.com/pjt3232/Business-Database",
+            problem: "The problem solved with this application is wanting to create a log of all of your employees and departments. This was solved by allowing the user to create, update, read, and/or delete any employee, manager, department, or salary. This app provides a drop down menu in the terminal that allows you to chose from various options, once you select an option it will use the user's input to update the entire log of the database. This allows you to manage how much money is going into each department, while also logging the user's entire business database.",
+            technologies: "MySQL and Inquirer npm package"
         },
         {
             title: "Weather Dashboard",
             imageUrl: "https://dochub.com/20peytra/oGZeMNnwXOb60ZeVQvbrYd/weather-dashboard-screenshot-png?dt=AZHBcx_FyJw6vB4h5oHy",
             deployedLink: "https://pjt3232.github.io/Weather-Dashboard/",
-            githubLink: "https://github.com/pjt3232/Weather-Dashboard"
+            githubLink: "https://github.com/pjt3232/Weather-Dashboard",
+            problem: "The problem solved with this application is wanting to know the weather in various locations from around the world. This was solved by allowing the user to select a city from anywhere in the world, which then displays a 5 day forecast of that city to the user. This app also allows the user to have a saved history of their past locations they had viewed, allowing convient access to prior search results.",
+            technologies: "OpenWeather API, jQuery, Bootstrap, and Day.js"
         },
     ];
 
@@ -59,6 +65,8 @@ const Portfolio = () => {
                                 <img variant="top" src={project.imageUrl} alt={project.title} className="project-img"/>
                                 <div>
                                     <h2>{project.title}</h2>
+                                    <p><strong>Problem/Solution: </strong>{project.problem}</p>
+                                    <p><strong>Technologies: </strong>{project.technologies}</p>
                                     <Link to={project.deployedLink} target="_blank">
                                         <button className="btn-primary">Deployed Application</button>{' '}
                                     </Link>
